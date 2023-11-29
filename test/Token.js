5b58d8ae3175fe944bcf611a45e7454d86764d1f
Joseph Reynolds <72422816+JosephJamesReynolds@users.noreply.github.com>
2023-11-29 05:39:18 -0800

    4. Token Balances

--- test/Token.js
+++ test/Token.js
@@ -5,12 +5,15 @@ const tokens = (n) => {
 	return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
-describe('Token', ()=> {
-	let token
-	
+describe('Token', () => {
+	let token, accounts, deployer
+
 	beforeEach(async () => {
 		const Token = await ethers.getContractFactory('Token')
 		token = await Token.deploy('Joseph Money', 'JMN', '1000000')
+
+		accounts = await ethers.getSigners()
+		deployer = accounts[0]
 	})
 
 	describe('deployment', () => {
@@ -30,6 +33,9 @@ describe('Token', ()=> {
 		})
 		it('has correct total supply', async () => {
 			expect(await token.totalSupply()).to.equal(totalSupply)
+		})
+		it('assigns total supply to deployer', async () => {
+			expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
 		})
 	})	
 })
